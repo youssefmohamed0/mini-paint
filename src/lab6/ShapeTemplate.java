070b79ecd604f021e5231f28defe972c9d473c77
youssef <youssefyoussef.m4@gmail.com>
2024-11-09 22:06:00 +0200

    Create abstract class to avoid repeated getters and setters

---
+++ src/lab6/ShapeTemplate.java
@@ -0,0 +1,61 @@
+package lab6;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Map;
+
+public abstract class ShapeTemplate implements Shape{
+    private Color color;
+    private Color FillColor;
+    private Point position;
+    private Map<String, Double> properties;
+
+    public ShapeTemplate(Color color, Color FillColor, Point position, Map<String, Double> properties) {
+        this.color = color;
+        this.FillColor = FillColor;
+        this.position = position;
+        this.properties = properties;
+    }
+    
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public Color getFillColor() {
+        return FillColor;
+    }
+
+    @Override
+    public void setFillColor(Color FillColor) {
+        this.FillColor = FillColor;
+    }
+
+    @Override
+    public Point getPosition() {
+        return position;
+    }
+
+    @Override
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+    @Override
+    public Map<String, Double> getProperties() {
+        return properties;
+    }
+
+    @Override
+    public void setProperties(Map<String, Double> properties) {
+        this.properties = properties;
+    }
+}
--- src/lab6/Square.java
+++ src/lab6/Square.java
@@ -6,18 +6,19 @@ import java.awt.Point;
 import java.util.Map;
 import javax.swing.text.Position;
 
-public class Square implements Shape {
+public class Square extends ShapeTemplate{
 //    private Double sideLength;
-    private Color color;
-    private Color FillColor;
-    private Point position;
-    private Map<String, Double> properties;
+//    private Color color;
+//    private Color FillColor;
+//    private Point position;
+//    private Map<String, Double> properties;
 
     public Square(Color color, Color FillColor, Point position, Map<String, Double> properties) {
-        this.color = color;
-        this.FillColor = FillColor;
-        this.position = position;
-        this.properties = properties;
+//        this.color = color;
+//        this.FillColor = FillColor;
+//        this.position = position;
+//        this.properties = properties;
+        super(color, FillColor, position, properties);    
     }
 
     @Override
@@ -27,52 +28,10 @@ public class Square implements Shape {
 
     @Override
     public void draw(Graphics canvas) {
-        int side = properties.get("sideLength").intValue();
-        canvas.setColor(color);
-        canvas.drawRect(position.x, position.y, side, side);
-        canvas.setColor(FillColor);
-        canvas.fillRect(position.x+1, position.y+1, side-1, side-1);
-    }
-    
-    @Override
-    public Color getColor() {
-        return color;
-    }
-
-    @Override
-    public void setColor(Color color) {
-        this.color = color;
-    }
-
-    @Override
-    public Color getFillColor() {
-        return FillColor;
-    }
-
-    @Override
-    public void setFillColor(Color FillColor) {
-        this.FillColor = FillColor;
-    }
-
-    @Override
-    public Point getPosition() {
-        return position;
-    }
-
-    @Override
-
-    public void setPosition(Point position) {
-        this.position = position;
-    }
-
-    @Override
-    public Map<String, Double> getProperties() {
-        return properties;
-    }
-
-    @Override
-    public void setProperties(Map<String, Double> properties) {
-        this.properties = properties;
-    }
-    
+        int side = getProperties().get("sideLength").intValue();
+        canvas.setColor(getColor());
+        canvas.drawRect(getPosition().x, getPosition().y, side, side);
+        canvas.setColor(getFillColor());
+        canvas.fillRect(getPosition().x+1, getPosition().y+1, side-1, side-1);
+    }    
 }
